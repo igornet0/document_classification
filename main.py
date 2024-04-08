5fe1f5090579b9c5aa021c5644ae55d6923596d3
Игорь Михайлец <892130836@mail.ru>
2024-04-08 12:09:53 +0300

    add files main

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from matplotlib import pyplot as plt
 from prepare_image import *
+import logging
 
-def get_data(batch_size, target_size=(400, 400)):
+logging.getLogger('tensorflow').disabled = True
+
+def get_data(batch_size, target_size=(250, 250), train_list=[]):
 
     data_path = get_data_path()
     classes = {}
@@ -15,54 +18,102 @@ def get_data(batch_size, target_size=(400, 400)):
     for i, x in enumerate(data_path["Украина"]):
         classes[x] = matrix[i]
 
-    for country, label in classes.items():
-        print(f"{country}: {label}")
-
     print(f"all_files={col_files(data_path, 'dataset')}")
-    images_labels = generater_images(data_path, classes, target_size=target_size, batch_size=batch_size)
-
+    images_labels = generater_images(data_path, classes, target_size=target_size, batch_size=batch_size, train_list=train_list)
     X = []
     y = []
-
     for image, label in images_labels:
         X.append(image)
         y.append(label)
-
+        if len(X) >= batch_size:
+            break
     X = np.array(X)
     y = np.array(y)
-    print(X[0].shape)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
 
+def get_data_test(target_size=(250, 250)):
+    data_path = get_data_path("test")
+    classes = {}
+    matrix = np.zeros((5, 5), dtype=int)
+    np.fill_diagonal(matrix, 1)
+    for i, x in enumerate(data_path["Test"]):
+        classes[x] = matrix[i]
+
+    print(f"all_files_test={col_files(data_path, 'test')}")
+    images_labels = generater_images(data_path, classes, dataset_path="test", target_size=target_size, batch_size=None)
+    return images_labels
+
 #23000 max
-X_train, X_test, y_train, y_test = get_data(10000)
 
 model = models.Sequential([
-    layers.Input(shape=(*X_train[0].shape, 1)),
-    layers.Conv2D(128, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
+    layers.Input(shape=(250, 250, 3)),
     layers.Conv2D(64, (3, 3), activation='relu'),
     layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Conv2D(32, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(32, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
     layers.Flatten(),
     layers.Dense(128, activation='relu'),
-    layers.Dense(len(y_train[0]))  # Количество уникальных классов
+    layers.Dropout(0.2),
+    layers.Dense(64),
+    layers.Dense(5, activation='softmax')  # Количество уникальных классов
 ])
 
 model.compile(optimizer='adam',
-              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
               metrics=['accuracy'])
 
+
 # Настройка EarlyStopping
+bath_size = 10000
+
+#model = tf.keras.models.load_model('classification_model_250-3.keras')
+
+
 early_stopping = EarlyStopping(monitor='val_loss',  # Следим за изменением loss на валидации
-                               patience=10,         # Количество эпох без улучшений после которых тренировка будет остановлена
+                               patience=3,         # Количество эпох без улучшений после которых тренировка будет остановлена
                                verbose=1,           # Будет печатать сообщение при остановке
                                mode='min',          # Остановка когда значение monitor перестанет уменьшаться
                                restore_best_weights=True)  # Восстановление весов с лучшего шага
 
+#"Миграционная карта", 'Паспорт', "Страница КПП"
+X_train, X_test, y_train, y_test = get_data(bath_size, train_list=[])
+
 history = model.fit(X_train, y_train,
-                    validation_data=(X_test, y_test),
-                    epochs=30,
+                    epochs=20,
+                    validation_data=(y_train, y_test),
                     callbacks=[early_stopping])
 
-model.save('classification_model_400.keras')
+model.save('classification_model_250-3.1.keras')
+
+data_path = get_data_path()
+classes = {}
+matrix = np.zeros((5, 5), dtype=int)
+np.fill_diagonal(matrix, 1)
+int_l = {}
+for i, x in enumerate(data_path["Украина"]):
+    classes[np.argmax(matrix[i])] = x
+    int_l[np.argmax(matrix[i])] = matrix[i]
+
+labels_images = {}
+n = 0
+
+for image, label in get_data_test():
+    image = image[0]
+    labels_images.setdefault(np.argmax(label), []).append(image)
+    n += 1
+    if n >= 400:
+        break
+
+
+for key, images in labels_images.items():
+    images = np.array(images)
+    labels = np.array([int_l[key] for _ in range(len(images))])
+    pred = model.predict(images, verbose=0)
+    data = model.evaluate(images, labels, verbose=0)
+    accuracy = data[1]
+    loss = data[0]
+    print(f"Loss for class {classes[key]}: {round(loss, 2)}")
+    print(f"Accuracy for class {classes[key]}: {round(accuracy, 2)}")
--- prepare_image.py
+++ prepare_image.py
@@ -4,49 +4,55 @@ import numpy as np
 from random import shuffle
 
 def prepare_data(image_path, target_size):
-
     image = cv2.imread(image_path)
     # Преобразовать изображение в черно-белое
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    #gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     
-    resized_image = cv2.resize(gray_image, target_size)
+    resized_image = cv2.resize(image, target_size)
 
     # Нормализовать значения пикселей до диапазона [0, 1]
     normalized_image = resized_image / 255.0
+    #print(normalized_image.shape)
     return normalized_image
 
 
-def generater_images(data_path, classes, dataset_path="dataset", target_size=(224, 224), batch_size=None):
+def rotate_image(image, degrees):
+    return cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)
+
+def generater_images(data_path, classes, dataset_path="dataset", target_size=(224, 224), batch_size=None, train_list=[]):
     if not batch_size is None:
+        l_batch_size = batch_size
         col_country_documents = batch_size // len(data_path.keys()) 
-    n = 0
     while True:
         for country, documents in data_path.items():
-            if batch_size < 0:
+            if not batch_size is None and batch_size < 0:
+                batch_size = l_batch_size
                 break
             path = os.path.join(dataset_path, country)
             for document in documents:
-                if batch_size < 0:
+                if not batch_size is None and batch_size < 0:
                     break
+                if document in train_list:
+                    continue
                 images_path = os.path.join(path, document)
                 list_image = os.listdir(images_path)
                 shuffle(list_image)
                 for i, image in enumerate(list_image):
-                    if batch_size < 0:
+                    if image == ".DS_Store":
+                        continue
+
+                    if not batch_size is None and batch_size <= 0:
                         break
+
                     image_path = os.path.join(images_path, image)
                     image = prepare_data(image_path, target_size)
-                    label = classes[document].tolist()
-                    if i >= col_country_documents // len(documents):
-                        break
-                    yield image, label
-                    n += 1
-                    if not batch_size is None and batch_size >= 0:
-                        batch_size -= 1
-        if batch_size < 0:
-            print(f"{n=}")
-            break
+                    label = classes[document]
+                    yield image,label
 
+                    if not batch_size is None and batch_size <= 0:
+                        batch_size -= 1
+        if not batch_size is None and batch_size <= 0:
+            break
                     
 def get_data_path(dataset_path="dataset"):
     country_paths = os.listdir(dataset_path)
